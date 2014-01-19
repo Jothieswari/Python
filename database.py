15ddeebce71869b1a129267144d47bcdd7e4b97a
Jothieswari <jothieswari.m@gmail.com>
2014-01-19 19:44:48 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,8 @@
+import mysql.connector
+cn=mysql.connector.connect(user="root",database="student",password="root")
+cr=cn.cursor()
+cr.execute("select * from mark")
+for(r,n,m,rs) in cr:
+ print("{0<5}|{1:^20}|{2:<5}|{3:>10}".format(r,n,m,rs))
+cr.close()
+cn.close()
